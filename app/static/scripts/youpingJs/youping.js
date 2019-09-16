c8243e8847d835dad4a4444954b7a068905f4bc9
yuzhenliu <1109763686@qq.com>
2019-09-16 20:30:08 +0800

    完成了限时抢购部分

--- app/static/scripts/youpingJs/youping.js
+++ app/static/scripts/youpingJs/youping.js
@@ -30,14 +30,14 @@ function changeColor() {
 changeColor();
 
 // 当滚动到一定高度时,z-navWrap 板块变成了粘性定位
-$(window).scroll(function(e) {
+$(window).scroll(function (e) {
   let $navWrap = $('.z-navWrap');
   let $classify = $('.classify')
   let scrollT = $(this).scrollTop();
   if (scrollT > 484) { // 临界值元素会闪烁
-    $navWrap.addClass('stickyWrap'); 
+    $navWrap.addClass('stickyWrap');
     $classify.css('display', 'block');
-  }else {
+  } else {
     $navWrap.removeClass('stickyWrap');
     $classify.css('display', 'none');
   }
@@ -127,7 +127,7 @@ function renderNavList() {
   let $lis = $('.nums li');
   let img1W = $imgs.eq(0).width();
   let imgIndex = 1; // 默认从第一张开始
-  let numIndex = 0;// imgIndex 比 numIndex + 1
+  let numIndex = 0; // imgIndex 比 numIndex + 1
   let timer1 = null;
 
   // 首先图片会停留在第一张
@@ -160,15 +160,15 @@ function renderNavList() {
   }
 
   // 左箭头点击
-  $left.click(function() {
+  $left.click(function () {
     clearInterval(timer1);
-    
-    imgIndex --;
+
+    imgIndex--;
     if (imgIndex < 0) {
-      imgIndex = $imgs.length -3;
+      imgIndex = $imgs.length - 3;
     }
 
-    numIndex --;
+    numIndex--;
     if (numIndex < 0) {
       numIndex = $lis.length - 1;
     }
@@ -181,58 +181,216 @@ function renderNavList() {
     autoMove();
   })
 
-    // 右箭头点击
-    $right.click(function() {
-      clearInterval(timer1);
-      
-      imgIndex++;
-      if (imgIndex >= $imgs.length) {
-        imgIndex = 2;
-      }
+  // 右箭头点击
+  $right.click(function () {
+    clearInterval(timer1);
 
-      numIndex++;
-      if (numIndex >= $lis.length) {
-        numIndex = 0;
-      }
-  
-      $lis.eq(numIndex).addClass('now').siblings().removeClass('now');
-      $main.animate({
-        scrollLeft: img1W * imgIndex
-      }, 10, 'swing');
-  
-      autoMove();
-    })
+    imgIndex++;
+    if (imgIndex >= $imgs.length) {
+      imgIndex = 2;
+    }
 
-    // 点击小圆点切换
-    $lis.click(function() {
+    numIndex++;
+    if (numIndex >= $lis.length) {
+      numIndex = 0;
+    }
 
-      clearInterval(timer1);
+    $lis.eq(numIndex).addClass('now').siblings().removeClass('now');
+    $main.animate({
+      scrollLeft: img1W * imgIndex
+    }, 10, 'swing');
 
-      numIndex = $(this).index();
-      imgIndex = numIndex + 1;
+    autoMove();
+  })
 
-      $lis.eq(numIndex).addClass('now').siblings().removeClass('now');
-      $main.animate({
-        scrollLeft: img1W * imgIndex
-      }, 10, 'swing');
+  // 点击小圆点切换
+  $lis.click(function () {
 
-      autoMove();
-      
-    })
+    clearInterval(timer1);
 
-})()
+    numIndex = $(this).index();
+    imgIndex = numIndex + 1;
+
+    $lis.eq(numIndex).addClass('now').siblings().removeClass('now');
+    $main.animate({
+      scrollLeft: img1W * imgIndex
+    }, 10, 'swing');
+
+    autoMove();
+
+  })
+
+})();
 
 
 // z-bannerWrap E
 
+// 每日新品
 // z-newPro S
 
 // z-newPro E
 
+// 小米众筹
 // z-crowdFunding S
-
-  
-
 
 // z-crowdFunding E 
 
+
+// 限时抢购
+// Z-timeLimited S
+// 限时抢购倒计时
+// 封装个倒计时函数：
+// 第一个时间对象: 现在的时间
+// 第二个时间对象：现在的时间 + 5 小时
+// 两个时间倒计时，每隔一秒动态渲染到页面上
+
+// 设置个倒计时，每隔固定时间(暂定 5 小时，调用一次倒计时函数)
+
+// 天: 24 * 60 * 60 * 1000
+// 小时: 60 * 60 * 1000
+// 分钟: 60 * 1000
+// 秒: 1000
+// 毫秒: 1
+
+// 这里本来应该存进 cookie 中去 或 localStorage 中，等待写
+
+(function () {
+
+  // 获取页面上的元素
+  let $hour = $('.hours');
+  let $minutes = $('.minutes');
+  let $seconds = $('.seconds');
+
+  let expectedHour = 5; // 存放倒计时时长
+  let timer = null; // 定时器
+
+  countdown(expectedHour); // 调用倒计时
+
+  // 倒计时函数
+  function countdown(expectedHour) {
+    let startDate = new Date().getTime(); // 开始时间
+    let endDate = new Date(startDate + expectedHour * 60 * 60 * 1000).getTime(); // 结束时间
+
+    // 设置一个定时器，每秒都去获取
+    clearInterval(timer);
+    timer = setInterval(function () {
+
+      let nowDate = new Date()
+      let nowDateTime = nowDate.getTime(); // 每秒的时间
+
+      let intervalTime = endDate - nowDateTime; // 间隔时间
+
+     if (intervalTime >= 0) {
+  
+      let hours = parseInt(intervalTime / (60 * 60 * 1000)); // 小时
+      let minutes = parseInt((intervalTime - (hours * 60 * 60 * 1000)) / (60 * 1000)); // 分钟
+      let seconds = 60 - nowDate.getSeconds(); // 秒钟
+
+      hours = hours > 9 ? hours : '0' + hours;
+      minutes = minutes > 9 ? minutes : '0' + minutes;
+      seconds = seconds > 9 ? seconds : '0' + seconds;
+
+      $hour.text(hours);
+      $minutes.text(minutes);
+      $seconds.text(seconds);
+
+     }else {
+      clearInterval(timer);
+     }
+
+    }, 1000);
+  }
+})();
+
+
+// 动态渲染列表  点击时候的样式还有些问题，暂时先这样吧
+(function() {
+
+  let $mainBox = $('.mainBox');
+  let $imgsBox = $('.imgsBox');
+  let $leftT = $('.leftT');
+  let $rightT = $('.rightT');
+  let sroLeft = 0;
+
+  if (sroLeft == 0) {
+    $leftT.mouseenter(function() {
+      $(this).css('background-position', '0 -1924px'); // 默认左边是不会显示可点击的图片的
+    })
+  }
+
+  // 发送 ajax 请求获取数据
+  $.ajax({
+    type: 'get',
+    url: '../static/scripts/youpingJs/timeItem.json',
+    dataType: 'json',
+    success: function (json) {
+      let tmpStr = '';
+      for(let i = 0; i < json.length; i ++) {
+        tmpStr += ` <li class="timeItem">
+        <img src="${json[i].imgUrl}" alt="" class="conImg">
+        <div class="con">
+          <h6 class="tit">${json[i].tit}</h6>
+          <p class="nowPrice">${json[i].nowPrice}<i class="oldPrice">${json[i].oldPrice}</i></p>
+        </div>
+      </li>`;
+      }
+      $imgsBox.html(tmpStr); // 动态渲染到页面上
+
+      // 点击左右按钮，每次偏移 timeItem 的长度为 270 px
+      // 点击左边，每次 $main 的 scrollLeft - 270
+      // 当 scrollLeft 为 0 时，禁止点击，显示蒙层 / return false
+      // 点击右边，每次 $main 的 scrollLeft + 270
+      // 当 scrollLeft 为 最大长度时(json.length * 270) 时，禁止点击，显示蒙层 return false
+
+      $leftT.click(function() {
+        // 先判断，再减
+        console.log(sroLeft);
+        if (sroLeft == 0) {
+          $leftT.mouseenter(function() {
+            $(this).css('background-position', '0 -1798px'); // 不是 0 则显示可点击
+          });
+          return false;
+        }
+        sroLeft -= 270;
+        $mainBox.animate({
+          scrollLeft: sroLeft
+        });
+        console.log(sroLeft);
+      });
+
+
+      $rightT.click(function() {
+
+        $leftT.mouseenter(function() {
+          $(this).css('background-position', '0 -1798px'); // 不是 0 则显示可点击
+        });
+        $leftT.mouseout(function() {
+          $(this).css('background-position', '0 -1924px'); // 不是 0 则显示可点击
+        });
+
+         // 先判断，再加
+        if (sroLeft == (json.length - 4) * 270) {
+          $rightT.css('background-position', '0 -1840px');
+          return false;
+        }
+        sroLeft += 270;
+        $mainBox.animate({
+          scrollLeft: sroLeft
+        });
+        console.log(sroLeft);
+      });
+
+
+
+
+
+    },
+    error: function (err) {
+      console.log(err);
+    }
+  });
+
+
+})();
+
+// Z-timeLimited E
