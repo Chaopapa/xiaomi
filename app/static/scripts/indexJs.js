438c2c20711c443fc8f94969dd7df9912a77429c
cc <1321607222@qq.com>
2019-09-17 19:57:19 +0800

    完成广告图模块基本功能

--- app/static/scripts/indexJs.js
+++ app/static/scripts/indexJs.js
@@ -8,4 +8,59 @@ $(function() {
     var right = $('.b-right');
     var num = $('.b-num li');
     _carousel(imgs, left, right, num, 6000);
+
+    /**
+     * banner图列表效果
+     */
+    var bannerNavList = {
+        target: $('.banner-nav ul'),
+
+        eventInit: function() {
+            this.show();
+            this.hide();
+        },
+
+        show: function() {
+            $(this.target).mouseover(function(e) {
+                var target = e.target;
+                if (target.nodeName == 'A') {
+                    $(target).next().css('display', 'block');
+                }
+            });
+        },
+
+        hide: function() {
+            var self = this;
+            $(this.target).mouseout(function(e) {
+                var target = e.target;
+                var toE = e.relatedTarget;
+                while (toE) {
+                    if ($(toE).hasClass('bn-show')) {
+                        $(toE).mouseout(function(e) {
+                            var toe = e.relatedTarget;
+                            var t = e.target;
+                            while (toe) {
+                                if ($(toe).hasClass('bn-show')) {
+
+                                    return;
+                                }
+                                toe = toe.parentNode;
+                            }
+                            if (target.nodeName == 'A') {
+                                $(target).next().css('display', 'none');
+                            }
+                        })
+                        return;
+                    }
+                    toE = toE.parentNode;
+                }
+
+                if (target.nodeName == 'A') {
+                    $(target).next().css('display', 'none');
+                }
+
+            });
+        }
+    };
+    bannerNavList.eventInit();
 });
