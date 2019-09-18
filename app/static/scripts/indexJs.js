d3b152949aa8e4e1797b3a1e194a290b79ee1dcc
cc <1321607222@qq.com>
2019-09-18 19:57:34 +0800

    完成小米秒杀模块基本功能

--- app/static/scripts/indexJs.js
+++ app/static/scripts/indexJs.js
@@ -63,4 +63,100 @@ $(function() {
         }
     };
     bannerNavList.eventInit();
+
+    /**
+     * 小米闪购轮播图
+     */
+    var flashCarousel = {
+        //轮播外层嵌套
+        c: $('.f-carousel'),
+        //内容盒子
+        box: $('.fc-goodsBox'),
+        //向左
+        left: $('.f-ctr-left'),
+        //向右
+        right: $('.f-ctr-right'),
+        //滚动条左卷距离
+        toLeft: 0,
+        //数据长度
+        dataL: 0,
+        //初始化化
+        init: function() {
+            this.loadData();
+            this.autoMove();
+            this.lastGoods();
+            this.nextGoods();
+        },
+        //定时器
+        timer: null,
+        //加载数据,x渲染页面
+        loadData: function() {
+            var self = this;
+            $.ajax({
+                type: 'get',
+                dataType: 'json',
+                url: '../static/api/indexApi/flashGoods.json',
+                cache: false,
+                success: function(data) {
+                    var text = '';
+                    self.dataL = data.length;
+
+                    $.each(data, function(index, item) {
+                        text += ' <div class="fcg-goods">' +
+                            '<img src="' + item.picture + '" alt="">' +
+                            '<div class="fcg-g-title">' + item.title + '</div>' +
+                            '<div class="fcg-g-desc">' + item.desc + '</div>' +
+                            '<p><span>' + item.nowPrice + '</span>元<del>' + item.beforePrice + '</del></p>' +
+                            '</div>'
+                    });
+                    self.box.html(text);
+                }
+            })
+        },
+
+        //轮播图自动移动
+        autoMove: function() {
+            clearInterval(this.timer);
+            this.timer = setInterval(function() {
+                this.toLeft += (this.c.width() + 14);
+                if (this.toLeft > (this.c.width() + 14) * (this.dataL / 4 - 1)) {
+                    this.toLeft = 0;
+                    this.c.scrollLeft(0);
+                    return;
+                }
+
+                this.c.scrollLeft(this.toLeft);
+
+            }.bind(this), 5000);
+
+        },
+        //向左滚动
+        lastGoods: function() {
+            this.left.click(function() {
+                clearInterval(this.timer);
+                this.toLeft -= (this.c.width() + 14);
+                if (this.toLeft < 0) {
+                    this.toLeft = (this.c.width() + 14) * (this.dataL / 4 - 1);
+                }
+                this.c.scrollLeft(this.toLeft);
+                this.autoMove();
+            }.bind(this));
+
+        },
+        //向右滚动
+        nextGoods: function() {
+            this.right.click(function() {
+                clearInterval(this.timer);
+                this.toLeft += (this.c.width() + 14);
+                if (this.toLeft > (this.c.width() + 14) * (this.dataL / 4 - 1)) {
+                    this.toLeft = 0;
+                }
+                this.c.scrollLeft(this.toLeft);
+                this.autoMove();
+            }.bind(this));
+        }
+
+
+    }
+    flashCarousel.init();
 });
